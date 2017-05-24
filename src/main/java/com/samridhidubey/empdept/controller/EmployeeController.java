406eeafb2d0a5cb7403a973be96cc1e6f6a82728
Samridhi Dubey <sd77574@gmail.com>
2017-05-24 10:51:12 +0200

    GH-6: all CRUD operations completed for employee

--- src/main/java/com/samridhidubey/empdept/controller/EmployeeController.java
+++ src/main/java/com/samridhidubey/empdept/controller/EmployeeController.java
@@ -39,4 +39,43 @@ public class EmployeeController {
 		}
 
 	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/update")
+	public ResponseEntity<?> update(int id, String empName, String empDateOfBirth, Double empSalary, String empAddress,
+			String empEmail, String empPhone, Integer deptId) {
+		Employee employee = new Employee();
+		employee.setId(id);
+		employee.setName(empName);
+		employee.setDateOfBirth(empDateOfBirth);
+		employee.setSalary(empSalary);
+		employee.setAddress(empAddress);
+		employee.setEmail(empEmail);
+		employee.setPhoneNumber(empPhone);
+		employee.setDepartmentId(deptId);
+		Boolean result = service.update(employee);
+		if (result) {
+			return new ResponseEntity<String>("Employee record update.", null, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Unable to update employee", null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/delete")
+	public ResponseEntity<?> delete(int id) {
+		Employee employee = new Employee();
+		employee.setId(id);
+		Boolean result = service.delete(employee);
+		if (result) {
+			return new ResponseEntity<String>("Employee record deleted.", null, HttpStatus.CREATED);
+		} else {
+			return new ResponseEntity<String>("Unable to delete employee", null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/find")
+	public Employee find(int id) {
+		Employee employee = new Employee();
+		employee.setId(id);
+		return service.findById(id);
+	}
 }
